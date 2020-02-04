9bdc6260f2ff311b8a63119c165729e0e3f15fdb
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-04 22:24:24 +0000

    coded cli.rb

--- lib/starships_cli.rb
+++ lib/starships_cli.rb
@@ -2,4 +2,4 @@ require_relative "./starships_cli/version"
 require_relative "./starships_cli/api"
 require_relative "./starships_cli/cli"
 require_relative "./starships_cli/starship"
-puts "environment file"
+
--- lib/starships_cli/cli.rb
+++ lib/starships_cli/cli.rb
@@ -1,6 +1,20 @@
 class StarshipsCli::CLI 
   def call 
-    puts "i'm in the cli"
+    puts "Welcome user!"
+    puts "To see starships, enter 'starships'"
+    puts "To exit the galaxy, enter 'exit'"
+    menu
   end 
   
+  def menu
+    input = gets.strip.downcase
+    if input == "starships"
+      puts "starships"
+      elsif input == "exit"
+      puts "exit"
+    else
+      puts "invalid entry,try again."
+      menu 
+    end
+  end
 end
